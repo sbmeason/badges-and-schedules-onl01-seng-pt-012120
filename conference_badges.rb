c9587471039474f580e343e8e7739ebb05bc4225
Samantha Meason <sbmeason@gmail.com>
2020-01-31 01:19:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,35 @@
-# Write your code here.
+people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(people)
+  "Hello, my name is #{people}."
+end
+
+def batch_badge_creator(people)
+  people.map do |name|
+    "Hello, my name is #{name}."
+  end
+end
+
+def assign_rooms(array)
+  nuarray = []
+  counter = 1
+  array.each do |name|
+    nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  return nuarray
+end
+
+def printer(array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end
+
+
+  
+
